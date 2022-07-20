c8793512752a8f6dae061702d3025b5e39dc7584
babuskaboi <duhanaygun3@gmail.com>
2022-07-20 13:12:45 +0300

    Create Main.java

---
+++ Not Ortalaması Hesaplayan Program/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+ public static void main(String[] args) {
+    //Değişken oluşturma
+    int mat,fizik,kimya,turkce,muzik;
+   Scanner inp= new Scanner(System.in);
+
+   
+    
+    
+ }
+    
+    
+}
