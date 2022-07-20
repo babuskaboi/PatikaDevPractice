56b1b64701c2d0235a6e15c038f46cf17fdcd087
babuskaboi <duhanaygun3@gmail.com>
2022-07-20 13:21:33 +0300

    Update Main.java

--- Not Ortalaması Hesaplayan Program/Main.java
+++ Not Ortalaması Hesaplayan Program/Main.java
@@ -1,15 +1,33 @@
 import java.util.Scanner;
 
-public class Main{
- public static void main(String[] args) {
-    //Değişken oluşturma
-    int mat,fizik,kimya,turkce,muzik;
-   Scanner inp= new Scanner(System.in);
+public class Main {
+  public static void main(String[] args) {
+    // Değişken oluşturma
+    int mat, fizik, kimya, turkce, muzik;
 
-   
+    //Scanner tanımlama
+    Scanner inp = new Scanner(System.in);
+
+    //Kullanıcıdan değer alma
+    System.out.println("Mat değerini giriniz:");
+    mat=inp.nextInt();
+
+    System.out.println("kimya değerini giriniz:");
+    kimya=inp.nextInt();
     
-    
- }
-    
-    
+    System.out.println("fizik değerini giriniz:");
+    fizik=inp.nextInt();
+
+    System.out.println("turkce değerini giriniz:");
+    turkce=inp.nextInt();
+
+    System.out.println("muzik değerini giriniz:");
+    muzik=inp.nextInt();
+
+    double notOrt=(mat+fizik+muzik+kimya+turkce)/5;
+
+    System.out.println("not ortalaması: "+notOrt);
+
+  }
+
 }
