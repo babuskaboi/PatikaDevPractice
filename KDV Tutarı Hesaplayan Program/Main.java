3f57b6609fd7b82b158de7468da71bb243af7ebd
babuskaboi <duhanaygun3@gmail.com>
2022-07-20 14:02:56 +0300

    Update Main.java

--- KDV Tutarı Hesaplayan Program/Main.java
+++ KDV Tutarı Hesaplayan Program/Main.java
@@ -1,27 +1,26 @@
 import java.util.Scanner;
 
-
-public class Main {
+public class main {
 
     public static void main(String[] args) {
-        double anaPara, Kdv, toplamtutar=0;
+        double anaPara, Kdv, toplamtutar = 0;
 
         Scanner inp = new Scanner(System.in);
 
+        // kullanıcıdan anapara tutarı alındı
         System.out.println("ana para girişi");
         anaPara = inp.nextInt();
 
+        // anaparaya göre kdv tutarı belirlendi
         if (anaPara > 0 && anaPara < 1000) {
             Kdv = 0.18;
+        } else {
+            Kdv = 0.08;
         }
-        else{
-            Kdv=0.08;
-        }
-        toplamtutar=anaPara+(Kdv*anaPara);
+        //kdv hesaplanıp ekrana yazdırıldı
+        toplamtutar = anaPara + (Kdv * anaPara);
 
-        System.out.println("son Tutar= "+toplamtutar);
-
-
+        System.out.println("son Tutar= " + toplamtutar);
 
     }
 
