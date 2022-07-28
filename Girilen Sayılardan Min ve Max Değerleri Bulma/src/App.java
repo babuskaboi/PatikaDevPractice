49b20a94b17db3c217bae47d4f9b8766afcb1708
Duhan Aygun <duhanaygun3@gmail.com>
2022-07-28 12:41:59 +0300

    first commit

---
+++ Girilen Sayılardan Min ve Max Değerleri Bulma/src/App.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        int i,valueNumber,minValue=1,maxValue=1;
+        int number;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kac Adet Sayi Girmek Istiyorsunuz?: ");
+        valueNumber = input.nextInt();
+
+        for (i =1; i<= valueNumber;i++){
+            System.out.print("Lutfen "+i+". sayiyi Giriniz: ");
+
+            number = input.nextInt();
+
+            if(i==1){
+                maxValue=number;
+                minValue=number;
+            }
+
+            if ( valueNumber ==1){
+
+                System.out.println("Sadece Bir Adet Sayi Girdiniz. En Buyuk ve En Kucuk Degerleri Bulmak " +
+                        "Icin Lutfen Birden Fazla Sayi Giriniz...");
+                        break;
+
+
+            }
+            if(number>maxValue){
+                maxValue = number;
+            }
+            if(number<minValue){
+                minValue =number;
+            }
+
+
+        }
+        System.out.println("En Buyuk Sayi: "+maxValue);
+        System.out.println("En Kucuk Sayi: "+minValue);
+
+
+
+
+    }
+}
