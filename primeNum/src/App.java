db7526716b7b127019ba3d313555ec569af85937
Duhan Aygun <duhanaygun3@gmail.com>
2022-07-31 15:53:52 +0300

    first commit

---
+++ primeNum/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+
+    static void asal(int sayi,int i){
+        if (i == sayi) {
+            System.out.print(sayi + " asal sayıdır.");
+            return;
+        } else if (sayi%i == 0) {
+            System.out.print(sayi + " asal sayı değildir.");
+            return;
+        }
+
+        asal(sayi, i + 1);
+
+    }
+    public static void main(String[] args) throws Exception {
+        asal(5,2);
+    }
+}
