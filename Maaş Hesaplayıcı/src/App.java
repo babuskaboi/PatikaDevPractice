1b7ce10c401ef46acea1330ab56b26c9c9f82ac0
Duhan Aygun <duhanaygun3@gmail.com>
2022-08-26 16:00:46 +0300

    first commit

---
+++ Maaş Hesaplayıcı/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+        Employee emp1=new Employee("duhan", "aygun",2000, 45, 1985);
+
+        emp1.showInfos();
+
+        
+
+    }
+}
