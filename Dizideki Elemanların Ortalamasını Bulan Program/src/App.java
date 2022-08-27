4ed45b69e04d92ab7b133fa0906a42982746fc83
Duhan Aygun <duhanaygun3@gmail.com>
2022-08-27 14:43:29 +0300

    first commit

---
+++ Dizideki Elemanların Ortalamasını Bulan Program/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int[] numbers = {1, 2, 3, 4, 5,6,7,8};
+        int sum = 0;
+        int total;
+        for (int i = 1; i < numbers.length; i++) {
+
+            sum += 1 / i ;
+        }
+        System.out.println("n(eleman sayısı) ==> " + numbers.length);
+        System.out.println("m(Harmonik seri) ==> " + sum);
+        total = numbers.length / sum ;
+
+        System.out.println("Harmonik avarage  n / m ==> "  + total);
+
+        
+       
+    }
+}
