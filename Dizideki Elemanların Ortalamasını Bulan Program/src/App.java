d09094b30400c5b44114cfad34a127e62926bb0c
Duhan Aygun <duhanaygun3@gmail.com>
2022-08-30 12:21:46 +0300

    first commit

--- Dizideki Elemanların Ortalamasını Bulan Program/src/App.java
+++ Dizideki Elemanların Ortalamasını Bulan Program/src/App.java
@@ -1,20 +1,35 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int[] numbers = {1, 2, 3, 4, 5,6,7,8};
-        int sum = 0;
-        int total;
-        for (int i = 1; i < numbers.length; i++) {
+        int[] list = { 15, 8, 6, 21, 12, 5 };
+        int number;
+        Arrays.sort(list);
 
-            sum += 1 / i ;
+        int sC = list[0];
+        int bC = list[list.length - 1];
+
+        System.out.println(list[0] + "-" + list[list.length - 1] + " aralıgında sayı giriniz: ");
+        Scanner inp = new Scanner(System.in);
+        number = inp.nextInt();
+        inp.close();
+
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] < number && Math.abs(number - list[i]) < Math.abs(number - sC)) {
+                sC = list[i];
+
+            }
+            if (list[i] > number && Math.abs(number - list[i]) < Math.abs(number - bC)) {
+                bC = list[i];
+
+            }
+
         }
-        System.out.println("n(eleman sayısı) ==> " + numbers.length);
-        System.out.println("m(Harmonik seri) ==> " + sum);
-        total = numbers.length / sum ;
 
-        System.out.println("Harmonik avarage  n / m ==> "  + total);
-
-        
-       
+        System.out.println("Girilen sayıdan küçük en yakın sayi: " + sC);
+        System.out.println("Girilen sayıdan büyük en yakın sayi: " + bC);
     }
 }
